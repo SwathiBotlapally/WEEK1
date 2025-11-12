4f5fb8b1ade99695a1d41d1448aacb8baf3a12dc
SwathiBotlapally <swathimudiraj39@gmail.com>
2025-11-12 20:19:08 +0530

    Update and rename README.md to intro.py

---
+++ intro.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your favorite hobby: ")
+print(f"Hello {name}! ")
+print(f"You are {age} years old and you love {hobby}.")
+print("Welcome to your Python learning journey!")
