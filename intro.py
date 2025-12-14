98adbbccf40e7a6aaa61fcb0cb0ae2ac9aa0d5be
SwathiBotlapally <swathimudiraj39@gmail.com>
2025-12-14 20:28:52 +0530

    Update and rename intro.py to personal_intro.py

--- intro.py
+++
@@ -1,7 +0,0 @@
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-hobby = input("Enter your favorite hobby: ")
-print(f"Hello {name}! ")
-print(f"You are {age} years old and you love {hobby}.")
-print("Welcome to your Python learning journey!")
